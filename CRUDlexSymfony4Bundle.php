6a3aec723947f5f7d8b5cf39db23e930a81d7665
Philip Lehmann-Boehm <philip@philiplb.de>
2018-06-26 15:03:53 +0200

    added the actual bundle class

---
+++ CRUDlexSymfony4Bundle.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace philiplb\CRUDlexSymfony4Bundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class CRUDlexSymfony4Bundle extends Bundle
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getParent()
+    {
+        return null;
+    }
+
+}
