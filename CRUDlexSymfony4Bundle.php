f2eae7063b7a9b647d9717d679c45223bdf99d07
Philip Lehmann-Boehm <philip@philiplb.de>
2018-06-26 17:18:16 +0200

    loading the locale resources on bundle boot

--- CRUDlexSymfony4Bundle.php
+++ CRUDlexSymfony4Bundle.php
@@ -2,7 +2,9 @@
 
 namespace philiplb\CRUDlexSymfony4Bundle;
 
+use CRUDlex\Service;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\Translation\Loader\YamlFileLoader;
 
 class CRUDlexSymfony4Bundle extends Bundle
 {
@@ -13,6 +15,20 @@ class CRUDlexSymfony4Bundle extends Bundle
     protected function setupTemplates()
     {
         $this->container->get('twig')->getLoader()->addPath(__DIR__.'/../CRUDlex/src/views/', 'crud');
+    }
+
+    /**
+     * Setups the available locales.
+     */
+    protected function setupLocales()
+    {
+        $translator = $this->container->get('translator');
+        $locales   = Service::getLocales();
+        $localeDir = __DIR__.'/../CRUDlex/src/locales';
+        $translator->addLoader('yaml', new YamlFileLoader());
+        foreach ($locales as $locale) {
+            $translator->addResource('yaml', $localeDir.'/'.$locale.'.yml', $locale);
+        }
     }
 
     /**
@@ -29,6 +45,7 @@ class CRUDlexSymfony4Bundle extends Bundle
     public function boot()
     {
         $this->setupTemplates();
+        $this->setupLocales();
     }
 
 }
