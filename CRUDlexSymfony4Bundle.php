8092fad5c5afc819b5f533cf023bb71408b367e6
Philip Lehmann-Boehm <philip@philiplb.de>
2018-06-26 17:06:31 +0200

    setting up templates on boot

--- CRUDlexSymfony4Bundle.php
+++ CRUDlexSymfony4Bundle.php
@@ -8,11 +8,28 @@ class CRUDlexSymfony4Bundle extends Bundle
 {
 
     /**
+     * Setups the templates.
+     */
+    protected function setupTemplates()
+    {
+        var_dump(__DIR__.'/../CRUDlex/src/views/'); exit(1);
+        $this->container->get('twig')->getLoader()->addPath(__DIR__.'/../CRUDlex/src/views/', 'crud');
+    }
+
+    /**
      * {@inheritdoc}
      */
     public function getParent()
     {
         return null;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function boot()
+    {
+        $this->setupTemplates();
     }
 
 }
