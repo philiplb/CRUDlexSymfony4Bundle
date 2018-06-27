98925a0e977fbd5e60be7644cfa8fa3d21299f6c
Philip Lehmann-Boehm <philip@philiplb.de>
2018-06-27 13:31:57 +0200

    added copyright notices to the source files

--- Twig/CRUDlexExtension.php
+++ Twig/CRUDlexExtension.php
@@ -1,4 +1,14 @@
 <?php
+
+/*
+ * This file is part of the CRUDlexSymfony4Bundle package.
+ *
+ * (c) Philip Lehmann-Böhm <philip@philiplb.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace philiplb\CRUDlexSymfony4Bundle\Twig;
 
 use CRUDlex\TwigExtensions;
