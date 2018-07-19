0023bb31319fda7e657072c17c9c7facf08e1d84
Philip Lehmann-Boehm <philip@philiplb.de>
2018-07-19 17:56:27 +0200

    removed commented code

--- DependencyInjection/CRUDlexSymfony4Extension.php
+++ DependencyInjection/CRUDlexSymfony4Extension.php
@@ -20,8 +20,6 @@ class CRUDlexSymfony4Extension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
-        //$container->setParameter('crudlex.datafactory', 'CRUDlex\\MySQLDataFactory');
-        //$container->setParameter('crudlex.file', '%kernel.project_dir%/crud.yml');
         $loader = new YamlFileLoader(
             $container,
             new FileLocator(__DIR__.'/../Resources/config')
